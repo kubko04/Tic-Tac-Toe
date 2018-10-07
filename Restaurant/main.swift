ceccaa907e6711c2b84a21f47357d27d18a3e191
Jakub <jakubklucovsky@jakubs-macbook-pro.local>
2018-10-07 12:46:11 +0200

    Initial Commit

---
+++ Restaurant/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Restaurant
+//
+//  Created by Jakub klučovský on 10/7/18.
+//  Copyright © 2018 2Builders. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
